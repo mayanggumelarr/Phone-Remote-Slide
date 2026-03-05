d694837ef6ceae5b737c68edf27ee142e6159aaf
Mayang Gumelar <gumelarmayang@gmail.com>
2026-03-05 14:29:59 +0700

    update

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route("/next")
 def next_slide():
+    print("NEXT SLIDE")
     pyautogui.press("right")
     return "Next Slide"
 
 @app.route("/prev")
 def prev_slide():
+    print("PREV SLIDE")
     pyautogui.press("left")
     return "Previous Slide"
 
